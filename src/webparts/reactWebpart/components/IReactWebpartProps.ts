86f028d90cb53a2e01af52a4e6739830ca0c8544
Anshadk <anshadkammaje786@gmail.com>
2025-04-26 22:34:26 +0530

    Remove some-folder from repository

--- src/webparts/reactWebpart/components/IReactWebpartProps.ts
+++
@@ -1,8 +0,0 @@
-export interface IReactWebpartProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-  users: any[]; // Ideally, define a proper interface for user data
-}
